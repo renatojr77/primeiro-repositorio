9c1b03d0a255c5102c3731c0ab67fc1902c71aef
renatojr77 <renatojr.lins13@gmail.com>
2021-03-19 21:40:39 -0300

    Adicionando aqrquivo loop.js

---
+++ loop.js
@@ -0,0 +1,14 @@
+for(let i = 0; i <= 10; i++){
+  console.log("Isso é um loop...")
+}
+
+console.log('')
+console.log('Outro exemplo:')
+
+function papagaio(texto){
+    for(let i=1;i<=5;i++){
+        console.log(texto)
+    }   
+}
+
+papagaio()
